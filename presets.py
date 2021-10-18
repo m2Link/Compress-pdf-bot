8eb6b52d6ad82440c4fe642c475365af51bb5226
m2Link <88938300+m2Link@users.noreply.github.com>
2021-10-18 22:33:04 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,11 +1,11 @@
 class Presets(object):
-    WELCOME_TXT = "<b>Hello.. {}</b>\n<i>I can compress the size of pdf docs. Send me a pdf document to see " \
+    WELCOME_TXT = "<b>Hello.. {}</b>\n<i>Iam A Simple PDF Compressor Bot. I Can Compress The Size Of PDF Files. Send Me A PDF File To See The Magic 😉\n\nMade With ❤ BY @M2Botz " \
                   "the magic !</i> "
     INVALID_FORMAT = "<b>Error:</b>\n<i>I can only compress pdf documents. Please make it sure, you have given me a " \
                      "valid document. Try again...</i>"
     WAIT_MESSAGE = "⌛️ <b>Processing</b> ⌛"
     DOWNLOAD_MSG = "⌛️ <b>Downloading</b> ⌛"
-    UPLOAD_MSG = "⌛️ <b>Uploading</b> ⌛"
+    UPLOAD_MSG = "📤 <b>Uploading</b> 📤"
     FINISHED_BAR = "◼️"
     UN_FINISHED_BAR = "◻️"
     FINISHED_DL = "<b>Success !</b>\n<i>Document downloaded successfully.</i>"
