b083069e26103cbbf3ebd5ace614eaeacc882fc6
m2Link <88938300+m2Link@users.noreply.github.com>
2021-10-18 23:19:39 +0530

    Update display_progress.py

--- support/display_progress.py
+++ support/display_progress.py
@@ -23,12 +23,12 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "{0}{1}\n𝐒𝐭𝐚𝐭𝐮𝐬   : {2}%\n".format(
+        progress = "{0}{1}\nsᴛᴀᴛᴜs   : {2}%\n".format(
             ''.join([Presets.FINISHED_BAR for i in range(math.floor(percentage / 10))]),
             ''.join([Presets.UN_FINISHED_BAR for i in range(10 - math.floor(percentage / 10))]),
             round(percentage, 2))
 
-        tmp = progress + "𝐏𝐫𝐨𝐜𝐞𝐬𝐬 : {0}  𝐎𝐟  {1}\n𝐒𝐩𝐞𝐞𝐝    : {2}/s\n𝐖𝐚𝐢𝐭𝐢𝐧𝐠 : {3}\n".format(
+        tmp = progress + "ᴘʀᴏᴄᴇss : {0}  of  {1}\nsᴘᴇᴇᴅ    : {2}/s\nᴡᴀɪᴛɪɴɢ : {3}\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
